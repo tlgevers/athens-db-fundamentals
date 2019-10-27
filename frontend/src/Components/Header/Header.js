3a12875d5c95d6b9fce288c03a1f3ba10a12b177
Trevor Gevers <trevor.gevers@ditoweb.com>
2019-10-27 17:12:22 -0500

    assets for data, sqlalchemy connect to mysql, revised db password and user access

---
+++ frontend/src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar, NavbarBrand } from 'shards-react'
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <Navbar type="dark" theme="primary" expand="md">
+                <NavbarBrand href="#">Athens</NavbarBrand>
+
+            </Navbar>
+        )
+    }
+}
